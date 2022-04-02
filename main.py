396abbb55fce2193a2cb51c36c9e692a68bf896c
Victor Podust <vpodust@toweriqua.com>
2022-04-02 23:17:27 +0300

    Beta

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ class Vertex:
 			yield vertex
 			vertex = vertex.next
 
+	def triangulate(self):
+		base_v = self.next
+		cur_v = base_v.next.next
+		while cur_v.v != self.v:
+			yield base_v.v, cur_v.v
+			cur_v = cur_v.next
+
 	def __repr__(self):
 		return f"{self.__class__.__name__}(v={self.v}, id={self.id}" \
 		       f", next={self.__class__.__name__}(v={self.next.v}, id={self.next.id}))"
@@ -61,10 +68,19 @@ def get_polygon_to_cut(
 		raise ValueError("Implementation error: no shared polygon")
 
 
-def cut(i: int, j: int, polygons: Polygons_T, relatives: Relatives_T) -> None:
+def do_cut(i: int, j: int, polygons: Polygons_T, relatives: Relatives_T) -> None:
 	"""Do cut between i-th and j-th initial vertices."""
 	pi = get_polygon_to_cut(i, j, relatives)
 	v1, v2 = relatives[i][pi], relatives[j][pi]
+
+	# Ensure that v2->v1 length <= v1->v2 length.
+	for u1, u2 in zip(v1, v2):
+		if u1.next.v == i:
+			v1, v2 = v2, v1
+			break
+		if u2.next.v == j:
+			break
+
 	v1_next, v2_next = Vertex(v2.v, next(vertex_id), v2.next), Vertex(v1.v, next(vertex_id), v1.next)
 	v1.next, v2.next = v1_next, v2_next
 
@@ -72,6 +88,9 @@ def cut(i: int, j: int, polygons: Polygons_T, relatives: Relatives_T) -> None:
 	polygons.append(v2)
 	new_pi = len(polygons) - 1
 	relatives[i][new_pi], relatives[j][new_pi] = v2_next, v1_next
+	v = v2.next
+	while v.v != v2_next.v:
+		relatives[v.v][new_pi] = relatives[v.v].pop(pi)
 
 
 def polygon_str(vertex: Vertex) -> str:
@@ -83,20 +102,27 @@ def show_polygons(polygons: Polygons_T) -> None:
 
 
 def main():
-	# n = int(input())
-	# k = int(input())
-
-	n = 8
-	k = 3
+	n = int(input())
+	k = int(input())
 
 	# polygons[i] is one of the vertices of i-th polygon.
 	# relatives[i] is dict with indices of polygons i-th initial vertex belongs
 	# to as keys and respective 'clones' of i-th initial vertex as values.
 	polygons, relatives = init_polygon(n)
 
-	cut(5, 1, polygons, relatives)
-	cut(1, 6, polygons, relatives)
-	show_polygons(polygons)
+	for _ in range(k):
+		i, j = map(int, input().split())
+		do_cut(i, j, polygons, relatives)
+
+	cuts = tuple(
+		itertools.chain.from_iterable(
+			vertex.triangulate()
+			for vertex in polygons
+		)
+	)
+	print(len(cuts))
+	for (i, j) in cuts:
+		print(i, j)
 
 
 if __name__ == "__main__":
